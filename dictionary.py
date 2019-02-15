398ee1ca44ae029baf1c83b356f2631ccb9aac60
wangchao <wangchao_yy@migu.cn>
2019-02-15 17:25:25 +0800

    commit

---
+++ dictionary.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+字典（dictionary）是Python中另一个非常有用的内置数据类型。
+
+列表是有序的对象集合，字典是无序的对象集合。两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+
+字典是一种映射类型，字典用 { } 标识，它是一个无序的 键(key) : 值(value) 的集合。
+
+键(key)必须使用不可变类型。
+
+在同一个字典中，键(key)必须是唯一的。
+'''
+
+dict ={}
+dict['one'] = "1-菜鸟教程"
+tinydict = {'name':'runoob','code':1,'site':'www.runoob.com'}
+#输出见为'one'的值
+print(dict['one'])
+#输出键为2的值
+print(dict[2])
+
+#输出完整的字典
+print(tinydict)
+
+#输出所有键
+print(tinydict.keys())
+#输出所有值
+print(tinydict.values())
+
+'''
+构造函数 dict() 可以直接从键值对序列中构建字典如下：
+>>>dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)])
+{'Taobao': 3, 'Runoob': 1, 'Google': 2}
+>>> {x: x**2 for x in (2, 4, 6)}
+{2: 4, 4: 16, 6: 36}
+>>> dict(Runoob=1, Google=2, Taobao=3)
+{'Runoob': 1, 'Google': 2, 'Taobao': 3}
+
+1、字典是一种映射类型，它的元素是键值对。
+2、字典的关键字必须为不可变类型，且不能重复。
+3、创建空字典使用 { }。
+'''
